680bade5e0c5effa2115b2683a273a8ae75c0af3
Kamil <kamil55525@gmail.com>
2020-03-08 20:50:17 +0100

    Add task3

---
+++ list2/task3_tolower.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+def to_lower(dir_path: str):
+    with os.scandir(dir_path) as x:
+        for entry in x:
+            entry: os.DirEntry
+            if entry.is_file():
+                print(os.path.join(os.path.dirname(entry.path), entry.name.lower()))
+                os.rename(entry.path, os.path.join(os.path.dirname(entry.path), entry.name.lower()))
+            elif entry.is_dir():
+                to_lower(entry.path)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Invalid number of arguments. Valid argument is: <directory name>")
+        exit(0)
+
+    to_lower(sys.argv[1])
