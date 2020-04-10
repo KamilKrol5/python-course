268bd5889d1db972aaa8da31f7b67e0ba8367a24
Kamil <kamil55525@gmail.com>
2020-04-10 21:31:19 +0200

    Fix task3

--- list4/task3_tree.py
+++ list4/task3_tree.py
@@ -3,51 +3,52 @@ from collections import deque
 
 
 class TreeNode:
-    def __init__(self, node_value, subtrees):
+    def __init__(self, node_value, subtrees, height):
+        self.height = height
         self.node_value = node_value
         self.subtrees = subtrees
 
     def __str__(self):
-        return f'{self.node_value} [{", ".join(str(s) for s in self.subtrees)}]'
+        return f'{self.node_value} h={self.height} [{", ".join(str(s) for s in self.subtrees)}]'
+
+    def dfs(self):
+        if self is None:
+            return
+        yield self.node_value
+        for st in self.subtrees:
+            yield from st.dfs()
+
+    def bfs(self):
+        if self is None:
+            return
+        queue = deque([self])
+        while queue:
+            st = queue.pop()
+            yield st.node_value
+            queue.extend(st.subtrees)
 
 
 def generate_random_tree(height: int):
     if height == 0:
         return None
     elif height == 1:
-        return TreeNode(random.randint(-(height ** 2), height ** 2), [])
+        return TreeNode(random.randint(-(height ** 2), height ** 2), [], 1)
 
-    subtrees_count = random.randint(1, 6)
+    subtrees_count = random.randint(1, 2)
     highest_subtree_position = random.randrange(0, subtrees_count)
     value = random.randint(-height ** 2, height ** 2)
-    subtrees = [
-        generate_random_tree(random.randint(1, height - 1))
-        if i != highest_subtree_position else
-        generate_random_tree(height - 1)
-        for i in range(subtrees_count)]
-    return TreeNode(value, subtrees)
-
-
-def dfs(tree: TreeNode):
-    if tree is None:
-        return
-    yield tree.node_value
-    for st in tree.subtrees:
-        yield from dfs(st)
-
-
-def bfs(tree):
-    if tree is None:
-        return
-    queue = deque([tree])
-    while queue:
-        st = queue.pop()
-        yield st.node_value
-        queue.extend(st.subtrees)
+    subtrees = []
+    for i in range(subtrees_count):
+        if i != highest_subtree_position:
+            _tree = generate_random_tree(random.randint(1, height - 1))
+        else:
+            _tree = generate_random_tree(height - 1)
+        subtrees.append(_tree)
+    return TreeNode(value, subtrees, height)
 
 
 if __name__ == '__main__':
     tree = generate_random_tree(3)
     print(tree)
-    print(list(dfs(tree)))
-    print(list(bfs(tree)))
+    print(list(tree.dfs()))
+    print(list(tree.bfs()))
