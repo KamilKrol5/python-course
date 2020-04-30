1f302be8ca1fd261d2bacd6ba668dd51738bcf11
Kamil <kamil55525@gmail.com>
2020-04-30 21:11:22 +0200

    Improve and fix task1

--- list5/task1_linear_regression/task1_linear_regression.py
+++ list5/task1_linear_regression/task1_linear_regression.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-m = 10
-
 # get id of 'Toy Story (1995)'
 movies = pd.read_csv('ml-latest-small/movies.csv')
 toy_story_id = movies[movies['title'] == 'Toy Story (1995)']['movieId'].to_numpy()[0]
@@ -11,28 +9,23 @@ print(f'Toy Story id: {toy_story_id}')
 
 # get users (user Ids) who rated Toy Story
 ratings = pd.read_csv('ml-latest-small/ratings.csv')
-users = ratings[ratings['movieId'] == toy_story_id]
-# print(users)
-
-# get ratings for Toy Story
-Y = users['rating'].to_numpy()
+users = ratings[ratings['movieId'] == toy_story_id][['rating', 'userId']].to_numpy()
+Y = users[:, 0]
 print(Y)
-user_ids = users['userId'].to_numpy()
-print(user_ids)
+# get all user ids
+user_ids = users[:, 1]
 
-# c = ratings[(ratings['movieId'] <= m) & (ratings['userId'].isin(user_ids))][['userId', 'movieId', 'rating']]
-c = ratings[(ratings['movieId'] <= m) & (ratings['userId'].isin(user_ids))]
-print(c)
-X = np.zeros(shape=(len(Y), m))
-for i, u_id in enumerate(user_ids, 0):
-    for j in range(0, m):
-        g = c[(c['userId'] == u_id) & (c['movieId'] == j+2)]['rating'].to_numpy()
-        print(g)
-        if len(g) > 0:
-            X[i][j] = g[0]
-print(X)
-print(X.shape)
-clf = LinearRegression().fit(X, Y)
-print(clf)
+
+def prepare_x(m):
+    ratings_ = ratings[(ratings['movieId'] <= m) & (ratings['userId'].isin(user_ids))]
+    X = ratings_.pivot(index='userId', columns='movieId', values='rating').to_numpy()[:, 1:]
+    np.nan_to_num(X, copy=False)
+    return X
+
+
+for m in [10,100,1000,10000]:
+    X = prepare_x(m)
+    clf = LinearRegression().fit(X, Y)
+    print(clf.score(X, Y))
 
 
