b82426f94a3e71ca604107ec4d7202b66c6820c8
Kamil <kamil55525@gmail.com>
2020-05-01 00:06:17 +0200

    Add chart, fixes, add learning

--- list5/task1_linear_regression/task1_linear_regression.py
+++ list5/task1_linear_regression/task1_linear_regression.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 # get id of 'Toy Story (1995)'
 movies = pd.read_csv('ml-latest-small/movies.csv')
 toy_story_id = movies[movies['title'] == 'Toy Story (1995)']['movieId'].to_numpy()[0]
+movie_ids = movies['movieId'].to_numpy()
 print(f'Toy Story id: {toy_story_id}')
 
 # get users (user Ids) who rated Toy Story
@@ -17,20 +18,46 @@ Y = users[:, 0]
 user_ids = users[:, 1]
 
 
-def prepare_x(max_book_id):
-    ratings_ = ratings[(ratings['movieId'] <= max_book_id) & (ratings['userId'].isin(user_ids))]
-    X = ratings_.pivot(index='userId', columns='movieId', values='rating').to_numpy()[:, 1:]
-    np.nan_to_num(X, copy=False)
-    return X
+def prepare_x(max_book_id, usr_ids):
+    indexes = movie_ids[movie_ids <= max_book_id]
+    ratings_ = ratings[(ratings['movieId'] <= max_book_id) & (ratings['userId'].isin(usr_ids))]
+    X_ = ratings_.pivot(index='userId', columns='movieId', values='rating').reindex(indexes, axis='columns').to_numpy()[:, 1:]
+
+    np.nan_to_num(X_, copy=False)
+    return X_
 
 
-chart_x = [10, 100, 500, 1000, 2500, 5000, 7500, 10000]
+chart_x = [10, 100, 200, 500, 1000, 2500, 5000, 7500, 10000]
 chart_y = []
 for m in chart_x:
-    X = prepare_x(m)
+    X = prepare_x(m, user_ids)
+    # print(X)
     clf = LinearRegression().fit(X, Y)
     chart_y.append(clf.score(X, Y))
 
-plt.plot(chart_x, chart_y)
+plt.plot(chart_x, chart_y, linewidth=8)
 plt.xscale('log')
+plt.xlabel('m')
+plt.ylabel('determination R^2')
 plt.show()
+
+# learning
+for m in chart_x:
+    X = prepare_x(m, user_ids[:-15])
+    clf = LinearRegression().fit(X, Y[:-15])
+    # prediction
+    X_for_prediction = prepare_x(m, user_ids[-15:])
+    predicted = clf.predict(X_for_prediction)
+    # print(f'Predicted: {predicted}')
+    # print(f'Expected:  {Y[-15:]}')
+    print(f'---m = {m}---\n(Expected, Predicted):\n{" ".join([str(pair) for pair in zip(Y[-15:], predicted)])}')
+
+    if m == 10000:
+        fig, ax = plt.subplots(1, 1)
+        ax.grid(True)
+        ax.scatter(np.arange(1, 16), predicted, c='coral', s=30, label='predicted')
+        ax.scatter(np.arange(1, 16), Y[-15:], c='green', s=50, label='expected')
+        ax.set_title('Regression model prediction results (m=10000)')
+        ax.legend()
+        fig.tight_layout()
+        plt.show()
