51175a7a517f1551aadefef545241001597a8d5e
Kamil <kamil55525@gmail.com>
2020-05-15 19:41:24 +0200

    Add tanh

--- list6/activation_function_utils.py
+++ list6/activation_function_utils.py
@@ -15,9 +15,14 @@ def _relu_gradient(x):
     return np.where(x > 0, 1, 0)
 
 
+def tanh_derivative(x):
+    return 1.0 - np.tanh(x)**2
+
+
 ActivationFunctionUtils = namedtuple('_ActivationFunctionUtils', ['function', 'derivative'])
 
 activation_functions_utils = {
     'sigmoid': ActivationFunctionUtils(function=sigmoid, derivative=_sigmoid_star),
-    'relu': ActivationFunctionUtils(function=_relu, derivative=_relu_gradient)
+    'relu': ActivationFunctionUtils(function=_relu, derivative=_relu_gradient).function,
+    'tanh': ActivationFunctionUtils(function=np.tanh, derivative=tanh_derivative)
 }
