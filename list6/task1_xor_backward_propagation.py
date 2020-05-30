0ef0e3ad4b2691f66ecac62b4bce2b4f88aa148f
Kamil <kamil55525@gmail.com>
2020-05-30 18:07:30 +0200

    Make task1 use neural_network.py, add 2variable tests

--- list6/task1_xor_backward_propagation.py
+++ list6/task1_xor_backward_propagation.py
@@ -1,93 +1,67 @@
 import numpy as np
 
 from activation_function_utils import activation_functions_utils
+from neural_network import NeuralNetwork, NeuralNetworkHiddenLayerInfo
 
 np.set_printoptions(suppress=True)
 
+networks2 = {
+    '2 sigmoid sigmoid': NeuralNetwork([
+        NeuralNetworkHiddenLayerInfo('sigmoid', 2),
+        NeuralNetworkHiddenLayerInfo('sigmoid', 4)], 1, eta=0.4),
+    '2 sigmoid relu': NeuralNetwork([
+        NeuralNetworkHiddenLayerInfo('sigmoid', 2),
+        NeuralNetworkHiddenLayerInfo('relu', 4)], 1, eta=0.4),
+    '2 relu sigmoid': NeuralNetwork([
+        NeuralNetworkHiddenLayerInfo('relu', 2),
+        NeuralNetworkHiddenLayerInfo('sigmoid', 4)], 1, eta=0.4),
+    '2 relu relu': NeuralNetwork([
+        NeuralNetworkHiddenLayerInfo('relu', 2),
+        NeuralNetworkHiddenLayerInfo('relu', 4)], 1, eta=0.4),
+}
 
-class NeuralNetwork:
-    _HIDDEN_LAYER_NEURONS_COUNT = 4
-
-    def __init__(self, training_data_sets, labels, activation_functions=('sigmoid', 'sigmoid')):
-        self.training_data_sets = training_data_sets
-        self.labels = labels
-        self.utils_input_to_layer1 = activation_functions_utils[activation_functions[0]]
-        self.weights_input_to_layer1 = np.random.rand(
-            self._HIDDEN_LAYER_NEURONS_COUNT,
-            self.training_data_sets.shape[1]
-        )
-        self.utils_layer1_to_output = activation_functions_utils[activation_functions[1]]
-        self.weights_layer1_to_output = np.random.rand(labels.shape[1], self._HIDDEN_LAYER_NEURONS_COUNT)
-        self.eta = 0.5
-        self.output = np.zeros(self.labels.shape)
-        self.layer1 = None
-        self.ready_for_prediction = False
-
-    def feed_forward(self):
-        self.layer1 = self.utils_input_to_layer1.function(
-            np.dot(self.training_data_sets, self.weights_input_to_layer1.T)
-        )
-        self.output = self.utils_layer1_to_output.function(np.dot(self.layer1, self.weights_layer1_to_output.T))
-
-    def back_propagation(self):
-        error_layer1_to_out = (self.labels - self.output) * self.utils_layer1_to_output.derivative(self.output)
-        weights_layer1_to_output_change = self.eta * np.dot(error_layer1_to_out.T, self.layer1)
-
-        error_input_to_layer1 = \
-            self.utils_input_to_layer1.derivative(self.layer1) * \
-            np.dot(error_layer1_to_out, self.weights_layer1_to_output)
-        weights_input_to_layer1_change = self.eta * np.dot(error_input_to_layer1.T, self.training_data_sets)
-
-        self.weights_input_to_layer1 += weights_input_to_layer1_change
-        self.weights_layer1_to_output += weights_layer1_to_output_change
-
-    def learn(self, iterations):
-        for _ in range(iterations):
-            self.feed_forward()
-            self.back_propagation()
-        self.ready_for_prediction = True
-
-    def predict(self, input_data_set):
-        if not self.ready_for_prediction:
-            raise RuntimeError('Neural network must learn before it can make any predictions')
-        layer1 = self.utils_input_to_layer1.function(np.dot(input_data_set, self.weights_input_to_layer1.T))
-        output = self.utils_layer1_to_output.function(np.dot(layer1, self.weights_layer1_to_output.T))
-        return output
+networks3 = {
+    '3 sigmoid sigmoid': NeuralNetwork([
+        NeuralNetworkHiddenLayerInfo('sigmoid', 3),
+        NeuralNetworkHiddenLayerInfo('sigmoid', 4)], 1, eta=0.4),
+    '3 sigmoid relu': NeuralNetwork([
+        NeuralNetworkHiddenLayerInfo('sigmoid', 3),
+        NeuralNetworkHiddenLayerInfo('relu', 4)], 1, eta=0.4),
+    '3 relu sigmoid': NeuralNetwork([
+        NeuralNetworkHiddenLayerInfo('relu', 3),
+        NeuralNetworkHiddenLayerInfo('sigmoid', 4)], 1, eta=0.4),
+    '3 relu relu': NeuralNetwork([
+        NeuralNetworkHiddenLayerInfo('relu', 3),
+        NeuralNetworkHiddenLayerInfo('relu', 4)], 1, eta=0.4),
+}
 
 
-def test_activation_functions(training_data_sets, labels, test_data_sets, test_labels, learning_iterations):
-    networks = {
-        'sigmoid sigmoid': NeuralNetwork(training_data_sets, labels, activation_functions=('sigmoid', 'sigmoid')),
-        'sigmoid relu': NeuralNetwork(training_data_sets, labels, activation_functions=('sigmoid', 'relu')),
-        'relu sigmoid': NeuralNetwork(training_data_sets, labels, activation_functions=('relu', 'sigmoid')),
-        'relu relu': NeuralNetwork(training_data_sets, labels, activation_functions=('relu', 'relu')),
-    }
+def test_activation_functions(
+        training_data_sets,
+        labels,
+        learning_iterations,
+        networks,
+        test_data_sets=None,
+        test_labels=None
+) -> None:
     for name, network in networks.items():
-        network.learn(learning_iterations)
+        network.learn(training_data_sets, labels, learning_iterations)
         error = np.square(labels - network.output).mean()
         print(f'Cost function value for TRAINING data for:\n"{name}": {error}')
         print(f'Got: {", ".join(str(x[0]) for x in network.output)}, '
               f'expected: {", ".join(str(x[0]) for x in labels)}\n')
 
-        network.predict(test_data_sets)
-        error2 = np.square(test_labels - network.output).mean()
-        print(f'Cost function value for TEST data for:\n "{name}": {error2}')
-        print(f'Got: {", ".join(str(x[0]) for x in network.output)}, '
-              f'expected: {", ".join(str(x[0]) for x in test_labels)}')
-        print('---')
-
-
-def default_test(data_, test_title=''):
-    print(test_title)
-    nn = NeuralNetwork(data_['training'], data_['training_labels'])
-    nn.learn(15000)
-    print(nn.predict(data_['training']))
-    print(nn.predict(data_['test']))
-    print('---')
+        if training_data_sets is not None and test_labels is not None:
+            network.predict(test_data_sets)
+            error2 = np.square(test_labels - network.output).mean()
+            print(f'Cost function value for TEST data for:\n "{name}": {error2}')
+            print(f'Got: {", ".join(str(x[0]) for x in network.output)}, '
+                  f'expected: {", ".join(str(x[0]) for x in test_labels)}')
+            print('---')
 
 
 if __name__ == '__main__':
-    data = {
+    data3 = {
         'xor': {
             'training': np.array([[0, 0, 1],
                                   [0, 1, 1],
@@ -126,35 +100,81 @@ if __name__ == '__main__':
         }
     }
 
-    # print('--- DEFAULT TEST ---')
-    # default_test(data['xor'], 'xor')
-    # default_test(data['and'], 'and')
-    # default_test(data['or'], 'or')
+    data2 = {
+        'xor': {
+            'training': np.array([[0, 0],
+                                  [0, 1],
+                                  [1, 0],
+                                  [1, 1]]),
+            'training_labels': np.array([[0], [1], [1], [0]]),
+        },
+        'and': {
+            'training': np.array([[1, 1],
+                                  [1, 0],
+                                  [0, 1],
+                                  [0, 0]]),
+            'training_labels': np.array([[1], [0], [0], [0]]),
+        },
+        'or': {
+            'training': np.array([[0, 0],
+                                  [0, 1],
+                                  [1, 0],
+                                  [1, 1]]),
+            'training_labels': np.array([[0], [1], [1], [1]]),
+        }
+    }
 
-    print('--- ACTIVATION FUNCTIONS TEST ---')
+    print('--- ACTIVATION FUNCTIONS TEST: 2 variables---')
     print('---------------------------------------------------------XOR:')
     test_activation_functions(
-        data['xor']['training'],
-        data['xor']['training_labels'],
-        data['xor']['test'],
-        data['xor']['test_labels'],
+        data2['xor']['training'],
+        data2['xor']['training_labels'],
         15000,
+        networks2
     )
 
     print('---------------------------------------------------------AND:')
     test_activation_functions(
-        data['and']['training'],
-        data['and']['training_labels'],
-        data['and']['test'],
-        data['and']['test_labels'],
+        data2['and']['training'],
+        data2['and']['training_labels'],
         15000,
+        networks2
     )
 
     print('---------------------------------------------------------OR:')
     test_activation_functions(
-        data['or']['training'],
-        data['or']['training_labels'],
-        data['or']['test'],
-        data['or']['test_labels'],
+        data2['or']['training'],
+        data2['or']['training_labels'],
         15000,
+        networks2
+    )
+    print('--- ACTIVATION FUNCTIONS TEST: 3 variables---')
+    print('---------------------------------------------------------XOR:')
+    test_activation_functions(
+        data3['xor']['training'],
+        data3['xor']['training_labels'],
+        15000,
+        networks3,
+        data3['xor']['test'],
+        data3['xor']['test_labels'],
+    )
+
+    print('---------------------------------------------------------AND:')
+    test_activation_functions(
+        data3['and']['training'],
+        data3['and']['training_labels'],
+        15000,
+        networks3,
+        data3['and']['test'],
+        data3['and']['test_labels'],
+    )
+
+    print('---------------------------------------------------------OR:')
+    test_activation_functions(
+        data3['or']['training'],
+        data3['or']['training_labels'],
+        15000,
+        networks3,
+        data3['or']['test'],
+        data3['or']['test_labels'],
     )
